d669801fd2607f2a40f518fec8a15439c45d107a
Ravi Khaniya <khaniyaravi999@gmail.com>
2024-12-07 18:47:06 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { navItems } from "@/Data";
-
+import dynamic from "next/dynamic";
 import Hero from "@/Components/Hero";
 import Grid from "@/Components/Grid";
 import Footer from "@/Components/Footer";
@@ -9,7 +9,10 @@ import Approach from "@/Components/Approach";
 import Experience from "@/Components/Experience";
 import RecentProjects from "@/Components/RecentProject";
 import { FloatingNav } from "@/Components/ui/FLoatingNav";
-
+const FloatingNav = dynamic(async () => {
+  const module = await import("@/Components/ui/FLoatingNav");
+  return module.FloatingNav;
+}, { ssr: false });
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
